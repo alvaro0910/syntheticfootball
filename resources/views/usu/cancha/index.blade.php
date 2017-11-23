6bd030e6b6dbcea96698653a3bb145bc84544bac
Alvaro Castaño <alvarocastano6@gmail.com>
2017-11-23 17:00:15 -0500

    Actualiacion Usu

--- resources/views/usu/cancha/index.blade.php
+++ resources/views/usu/cancha/index.blade.php
@@ -3,63 +3,63 @@
 @section('title', 'Lista Canchas')
 
 @section('content')
-  <div class="container">
-    <div class="row">
-      <div class="col-md-8 col-md-offset-2">
-        <div class="panel panel-default">
-          <div class="panel-heading">Lista de Canchas</div>
-            <div class="panel-body">
-              <table class="table">
-              <thead class="thead-inverse">
-              <tr>
-                <th>Cancha ID</th>
-                <th>Nombre</th>
-                <th>Ver</th>
-                <th>Reservar</th>
-              </tr>
-              </thead>
-              <tfoot>
-                <tr>
-                  <td colspan="2" style="text-align:center"><a title="Regresar" href="{{ route('admin') }}" >Regresar <img src="{{ asset('imgs/regresar.png') }}" alt="ver" style="width:25px;"/></a></td>
-                </tr>
-              <tfoot>
-              <tbody>
-                  @foreach ($list as $e)
-                    </tr>
-                    <tr>
-                    <td>{{ $e->id }}</td>
-                    <td><p>{{ $e->nombre }}</p></td>
-                    <td><a title="Ver" href="{{ route('canchasusu.show', $e) }}"><img src="{{ asset('imgs/ver.png') }}" alt="ver" style="width:25px;"/></a></td>
-                    <td><a title="Reservar" href="{{ route('reservasusu.create', $e->id) }}"><img src="{{ asset('imgs/editar.png') }}" alt="editar" style="width:25px;"/></a></td>
-                    </tr>
-                    <!--<p>{ $e->user->nombre }}</p>-->
-                  @endforeach
-                  </tbody>
-                  </table>
-                  </div>
-                </div>
-              </div>
-            </div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="panel panel-default">
+      <div class="panel-heading">Lista de Canchas</div>
+        <div class="panel-body">
 
-            <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-            <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
-            <script>
-            @if(Session::has('message'))
-              var type = "{{ Session::get('alert-type', 'info') }}";
-              switch(type){
-                case 'info':
-                  toastr.info("{{ Session::get('message') }}");
-                  break;
-                case 'warning':
-                  toastr.warning("{{ Session::get('message') }}");
-                  break;
-                case 'success':
-                  toastr.success("{{ Session::get('message') }}");
-                  break;
-                case 'error':
-                  toastr.error("{{ Session::get('message') }}");
-                  break;
-              }
-            @endif
-            </script>
+          <table class="table">
+            <thead class="thead-inverse">
+            <tr>
+              <th>Cancha ID</th>
+              <th>Nombre</th>
+              <th>Ver</th>
+              <th>Reservar</th>
+            </tr>
+            </thead>
+            <tfoot>
+              <tr>
+                <td colspan="2" style="text-align:center"><a title="Regresar" href="{{ route('admin') }}" >Regresar <img src="{{ asset('imgs/regresar.png') }}" alt="ver" style="width:25px;"/></a></td>
+              </tr>
+            <tfoot>
+            <tbody>
+              @foreach ($list as $e)
+              <tr>
+                <td>{{ $e->id }}</td>
+                <td><p>{{ $e->nombre }}</p></td>
+                <td><a title="Ver" href="{{ route('canchasusu.show', $e->id) }}"><img src="{{ asset('imgs/ver.png') }}" alt="ver" style="width:25px;"/></a></td>
+                <td><a title="Reservar" href="{{ route('reservasusu.create', $e->id) }}"><img src="{{ asset('imgs/reserva.png') }}" alt="editar" style="width:25px;"/></a></td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+  <script>
+    @if(Session::has('message'))
+      var type = "{{ Session::get('alert-type', 'info') }}";
+      switch(type){
+        case 'info':
+          toastr.info("{{ Session::get('message') }}");
+          break;
+        case 'warning':
+          toastr.warning("{{ Session::get('message') }}");
+          break;
+        case 'success':
+          toastr.success("{{ Session::get('message') }}");
+          break;
+        case 'error':
+          toastr.error("{{ Session::get('message') }}");
+          break;
+      }
+    @endif
+  </script>
 @stop
